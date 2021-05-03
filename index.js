22aff110b5c505686d1926e6a3b6324b832f01d1
HimanshuBohra <himanshupbohra@gmail.com>
2021-05-03 21:37:58 +0530

    test

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-const express = require('express')
-const app = express();
-const port = 8000;
+// const express = require('express')
+// const app = express();
+// const port = 8000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
+// app.route('/Create').get(function(req,res)
+// {
+//     res.send("Create Room!!");
+// });
+// app.route('/Join').get(function(req,res)
+// {
+//     res.send("Join Room!!");
+// });
+// app.get('/',function(req,res){
+//     res.send('Welcome to Isshoni!!');
+// });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
-});
+    app.set('view engine','pug');
+    app.get('/',function(req,res){
+        res.render('index',{
+            title:'Isshoni',message1:'Welcome Friend!',message2:'Ready to Create a Group'
+        });
+    });
+
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}!`)
+    });
